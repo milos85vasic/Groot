011b1744572a983a08a09dddda461498f55c230f
Milos Vasic <milos85vasic@gmail.com>
2017-03-01 13:24:59 +0100

    Groot: Initial setup.

---
+++ buildSrc/groot/src/main/groovy/plugina/Groot.groovy
@@ -0,0 +1,12 @@
+package groot
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class Groot implements Plugin<Project> {
+
+    void apply(Project project) {
+      println "It's ok."
+    }
+
+}
