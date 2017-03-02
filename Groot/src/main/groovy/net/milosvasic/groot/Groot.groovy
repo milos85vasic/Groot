6b0da248acec35d13cb7d5d28c68fb2d1afceb70
Milos Vasic <milos85vasic@gmail.com>
2017-03-02 09:40:50 +0100

    Groot: Initial plugin routines.

---
+++ Groot/src/main/groovy/net/milosvasic/groot/Groot.groovy
@@ -0,0 +1,48 @@
+package net.milosvasic.groot
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class Groot implements Plugin<Project> {
+
+    void apply(Project project) {
+//        project.extensions.create("kotlin", KotlinDefinition)
+        String version = "1.0.6"
+        project.apply(plugin: "java")
+        project.apply(plugin: "kotlin")
+        project.buildscript {
+            repositories {
+                jcenter()
+                mavenCentral()
+            }
+//            dependencies.classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+//            dependencies.classpath "org.jetbrains.kotlin:kotlin-reflect:$version"
+        }
+        project.repositories {
+            jcenter()
+            mavenCentral()
+        }
+        project.dependencies {
+            compile "junit:junit:4.12"
+            testCompile "junit:junit:4.12"
+
+            compile "org.jetbrains.kotlin:kotlin-stdlib:$version"
+            compile "org.jetbrains.kotlin:kotlin-reflect:$version"
+
+            testCompile "org.jetbrains.kotlin:kotlin-test:$version"
+            testCompile "org.jetbrains.kotlin:kotlin-test-junit:$version"
+        }
+
+//        project.task('hello') {
+//            doLast {
+//                println project.greeting.message
+//            }
+//        }
+    }
+
+//    class KotlinDefinition {
+//        String version = "1.0.6"
+//    }
+
+}
