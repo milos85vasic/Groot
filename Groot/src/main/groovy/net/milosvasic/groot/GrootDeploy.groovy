82fc587528ea001b3238b4d1bb18950fce6770dd
Milos Vasic <milos85vasic@gmail.com>
2017-03-03 13:14:40 +0100

    Groot: Working on deployment.

---
+++ Groot/src/main/groovy/net/milosvasic/groot/GrootDeploy.groovy
@@ -0,0 +1,24 @@
+package net.milosvasic.groot
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class GrootDeploy implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+
+//    class Deploy {
+//
+//        private Project project
+//
+//        Deploy(Project project) {
+//            this.project = project
+//        }
+//
+//    }
+
+}
