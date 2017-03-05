ca6f320107ff10542d9bd1294c01fd48063844b2
Milos Vasic <milos85vasic@gmail.com>
2017-03-05 11:07:59 +0100

    Groot: Updated Java tryout app.

--- Tryout_Java/src/main/java/net/milosvasic/tryout/groot/java/Main.java
+++ Tryout_Java/src/main/java/net/milosvasic/tryout/groot/java/Main.java
@@ -2,8 +2,14 @@ package net.milosvasic.tryout.groot.java;
 
 public class Main {
 
-    void main(String[] args){
-        System.out.println("Java test.");
+    public static void main(String[] args) {
+        System.out.println(
+                String.format(
+                        "Java tryout [ %s ][ %s ]",
+                        BuildConfig.NAME,
+                        BuildConfig.VERSION
+                )
+        );
     }
 
 }
