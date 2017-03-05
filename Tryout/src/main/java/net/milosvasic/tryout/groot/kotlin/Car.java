c16b4e957c72bb5f5294b2e2b59f8525466bbf04
Milos Vasic <milos85vasic@gmail.com>
2017-03-05 11:19:15 +0100

    Groot: Updated Kotlin tryout app.

---
+++ Tryout/src/main/java/net/milosvasic/tryout/groot/kotlin/Car.java
@@ -0,0 +1,21 @@
+package net.milosvasic.tryout.groot.kotlin;
+
+public class Car {
+
+    private String fuel;
+    private String brand;
+
+    public Car(String brand, String fuel) {
+        this.brand = brand;
+        this.fuel = fuel;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "fuel='" + fuel + '\'' +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+
+}
--- Tryout/src/main/kotlin/net/milosvasic/tryout/groot/kotlin/Main.kt
+++ Tryout/src/main/kotlin/net/milosvasic/tryout/groot/kotlin/Main.kt
@@ -7,13 +7,17 @@ private class Factory : ProjectFactory()
 
 fun main(args: Array<String>) {
 
+    println("Trying out Groot.")
+    println("${BuildConfig.NAME} ${BuildConfig.VERSION}")
+
     val TAG = Factory::class
     val logger = ConsoleLogger()
 
     val items = mutableListOf("Lion", "Elephant")
     items.add("Cow")
-    println("Trying out Groot.")
     items.forEachIndexed { i, s -> logger.c(TAG, s) }
-    println("${BuildConfig.NAME} ${BuildConfig.VERSION}")
+
+    val car = Car("Fiat", "Benzin")
+    logger.n(TAG, car.toString())
 
 }
