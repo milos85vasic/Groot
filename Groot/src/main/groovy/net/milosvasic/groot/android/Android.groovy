cc8c035cc1691fa7a33fec860257810277ef7730
Milos Vasic <milos85vasic@gmail.com>
2017-03-07 09:21:46 +0100

    Groot: Working on Android plugin.

---
+++ Groot/src/main/groovy/net/milosvasic/groot/android/Android.groovy
@@ -0,0 +1,13 @@
+package net.milosvasic.groot.android
+
+import net.milosvasic.groot.languages.java.Java
+import org.gradle.api.Project
+
+
+class Android extends Java {
+
+    Android(Project project) {
+        super(project)
+    }
+
+}
---
+++ Groot/src/main/groovy/net/milosvasic/groot/android/GrootAndroid.groovy
@@ -0,0 +1,14 @@
+package net.milosvasic.groot.android
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+class GrootAndroid implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+
+}
