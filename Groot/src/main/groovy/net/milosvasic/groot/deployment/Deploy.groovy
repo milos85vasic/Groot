7b6543e8f87cba8c80d48db6a3a8ea9d5aebcc1a
Milos Vasic <milos85vasic@gmail.com>
2017-03-13 08:00:41 +0100

    Groot: Android, covering ‘no flavors’ case.

--- Groot/src/main/groovy/net/milosvasic/groot/deployment/Deploy.groovy
+++ Groot/src/main/groovy/net/milosvasic/groot/deployment/Deploy.groovy
@@ -27,56 +27,102 @@ class Deploy {
             deployerJars "org.apache.maven.wagon:wagon-ftp:2.2"
         }
         if (project.hasProperty("android")) {
-            project.android.productFlavors.each {
-                flavor ->
-                    project.android.buildTypes.each {
-                        buildType ->
-                            String filetype = "aar"
-                            if (project.android.hasProperty("applicationVariants")) {
-                                filetype = "apk"
-                            }
-                            String capitalized = "${buildType.name}".substring(0, 1).toUpperCase()
-                            capitalized += "${buildType.name}".substring(1, "${buildType.name}".length())
-                            String variantName = "${flavor.name}$capitalized"
-                            String artifactName = "${project.name}_${projectVersion}"
-                            String fileName = new StringBuilder()
-                                    .append(project.buildDir)
-                                    .append(File.separator)
-                                    .append("outputs")
-                                    .append(File.separator)
-                                    .append(filetype)
-                                    .append(File.separator)
-                                    .append("$variantName")
-                                    .toString()
-                            File artifactFile = new File(fileName, "${artifactName}.$filetype")
-                            project.artifacts {
-                                archives(artifactFile) {
-                                    name variantName
-                                }
-                            }
-                            project.uploadArchives {
-                                repositories {
-                                    mavenDeployer {
-                                        repository(url: "ftp://${ftp.host}") {
-                                            authentication(
-                                                    userName: ftp.username,
-                                                    password: ftp.password
-                                            )
-                                        }
-                                        configuration = project.configurations.deployerJars
-                                        addFilter(variantName) {
-                                            artifact, file ->
-                                                artifact.name == variantName
-                                        }
-                                        pom(variantName).name = project.name
-                                        pom(variantName).groupId = projectPackage
-                                        pom(variantName).version = "${variantName}_$projectVersion"
-                                        pom(variantName).artifactId = project.name
-                                        pom(variantName).packaging = filetype
+            String filetype = "aar"
+            if (project.android.hasProperty("applicationVariants")) {
+                filetype = "apk"
+            }
+            if (project.android.productFlavors.size() > 0) {
+                project.android.productFlavors.each {
+                    flavor ->
+                        project.android.buildTypes.each {
+                            buildType ->
+                                String capitalized = "${buildType.name}".substring(0, 1).toUpperCase()
+                                capitalized += "${buildType.name}".substring(1, "${buildType.name}".length())
+                                String variantName = "${flavor.name}$capitalized"
+                                String artifactName = "${project.name}_${projectVersion}"
+                                String fileName = new StringBuilder()
+                                        .append(project.buildDir)
+                                        .append(File.separator)
+                                        .append("outputs")
+                                        .append(File.separator)
+                                        .append(filetype)
+                                        .append(File.separator)
+                                        .append("$variantName")
+                                        .toString()
+                                File artifactFile = new File(fileName, "${artifactName}.$filetype")
+                                project.artifacts {
+                                    archives(artifactFile) {
+                                        name variantName
                                     }
                                 }
+                                project.uploadArchives {
+                                    repositories {
+                                        mavenDeployer {
+                                            repository(url: "ftp://${ftp.host}") {
+                                                authentication(
+                                                        userName: ftp.username,
+                                                        password: ftp.password
+                                                )
+                                            }
+                                            configuration = project.configurations.deployerJars
+                                            addFilter(variantName) {
+                                                artifact, file ->
+                                                    artifact.name == variantName
+                                            }
+                                            pom(variantName).name = project.name
+                                            pom(variantName).groupId = projectPackage
+                                            pom(variantName).version = "${variantName}_$projectVersion"
+                                            pom(variantName).artifactId = project.name
+                                            pom(variantName).packaging = filetype
+                                        }
+                                    }
+                                }
+                        }
+                }
+            } else {
+                project.android.buildTypes.each {
+                    buildType ->
+                        String variantName = "${buildType.name}"
+                        String artifactName = "${project.name}_${projectVersion}"
+                        String fileName = new StringBuilder()
+                                .append(project.buildDir)
+                                .append(File.separator)
+                                .append("outputs")
+                                .append(File.separator)
+                                .append(filetype)
+                                .append(File.separator)
+                                .append("$variantName")
+                                .toString()
+                        File artifactFile = new File(fileName, "${artifactName}.$filetype")
+                        project.artifacts {
+                            archives(artifactFile) {
+                                name variantName
                             }
-                    }
+                        }
+                        project.uploadArchives {
+                            repositories {
+                                mavenDeployer {
+                                    repository(url: "ftp://${ftp.host}") {
+                                        authentication(
+                                                userName: ftp.username,
+                                                password: ftp.password
+                                        )
+                                    }
+                                    configuration = project.configurations.deployerJars
+                                    addFilter(variantName) {
+                                        artifact, file ->
+                                            artifact.name == variantName
+                                    }
+                                    pom(variantName).name = project.name
+                                    pom(variantName).groupId = projectPackage
+                                    pom(variantName).version = "${variantName}_$projectVersion"
+                                    pom(variantName).artifactId = project.name
+                                    pom(variantName).packaging = filetype
+                                }
+                            }
+                        }
+                }
+
             }
         } else {
             project.uploadArchives {
